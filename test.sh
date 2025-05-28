1df950ef35ad0816c3918b7e94cc47770aa435d5
Shaurya Gaur <shauryavrat@live.com>
2025-05-28 12:57:33 +0200

    create test script for f1

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=1200
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8192
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --output=test.out
+#SBATCH --error=test.err
+#SBATCH --job-name=testResTS
+#SBATCH --mail-user=shaurya.gaur@wur.nl
+#SBATCH --mail-type=ALL
+
+
+source /lustre/backup/WUR/WFSR/gaur001/venv/bin/activate
+python3 /lustre/backup/WUR/WFSR/gaur001/ResTS/test.py
+deactivate
